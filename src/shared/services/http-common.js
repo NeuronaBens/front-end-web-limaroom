e5b90cd890478e055a54737845839e843d28b3a5
renato-17 <renato.avela.17@gmail.com>
2022-10-24 00:03:25 -0500

    fix[project]: Fix deployed backend url

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://meetyouroommate-backend.herokuapp.com/',
+  baseURL: 'https://meetyouroommate-backend.herokuapp.com/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
