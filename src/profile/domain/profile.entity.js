ff617e00b8b170feebe23232e09374923f4ba472
renato-17 <renato.avela.17@gmail.com>
2022-11-17 15:35:07 -0500

    feat[profile]: Adding aditional information to profile and function to upload profile image

--- src/profile/domain/profile.entity.js
+++ src/profile/domain/profile.entity.js
@@ -1,8 +1,12 @@
 export default class Profile {
-  constructor ({ id = '', name = '', surname = '', phone = { number: '', code: '' } }) {
+  constructor ({ id = 0, name = '', surname = '', photoUrl = '', gender = '', about = '', location = '', phone = { number: '', code: '' } } = {}) {
     this.id = id
     this.name = name
     this.surname = surname
+    this.photoUrl = photoUrl
+    this.gender = gender
+    this.about = about
+    this.location = location
     this.phone = phone
   }
 }
--- src/profile/services/profiles-api.service.js
+++ src/profile/services/profiles-api.service.js
@@ -1,4 +1,5 @@
 import http from '@/shared/services/http-common'
+import { uploadImage } from '@/shared/config/firebase/storage'
 
 export default class ProfilesService {
   getAll () {
@@ -13,18 +14,21 @@ export default class ProfilesService {
     return http.get(`/profiles/${id}`)
   }
 
-  create (data, userId) {
-     
-    return http.post(`/profiles?userId=${userId}`, JSON.stringify(data))
-      .then(response => {
-        if (response.data.resource) {
-          const user = JSON.parse(localStorage.getItem('user'))
-          user.hasProfile = true
-          user.profileId = response.data.resource.id
-          localStorage.setItem('user', JSON.stringify(user))
-          return response.data.resource
-        }
-        return null
+  create (image, { profile, userId }) {
+    return uploadImage(`profiles/${userId}`, image)
+      .then((downloadUrl) => {
+        profile.photoUrl = downloadUrl
+        return http.post(`/profiles?userId=${userId}`, JSON.stringify(profile))
+          .then(response => {
+            if (response.data.resource) {
+              const user = JSON.parse(localStorage.getItem('user'))
+              user.hasProfile = true
+              user.profileId = response.data.resource.id
+              localStorage.setItem('user', JSON.stringify(user))
+              return response.data.resource
+            }
+            return null
+          })
       })
   }
 
