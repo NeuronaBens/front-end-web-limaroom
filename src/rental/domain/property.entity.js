f1af60478b6616376f947b2eea071d56964cb137
renato-17 <renato.avela.17@gmail.com>
2022-11-20 23:42:26 -0500

    file[rental]: Adding features icons

--- src/rental/domain/property.entity.js
+++ src/rental/domain/property.entity.js
@@ -1,10 +1,11 @@
 export default class Property {
-  constructor ({ id = 0, title = '', description = '', propertyType = '', location = '', assets = [] } = {}) {
+  constructor ({ id = 0, title = '', description = '', propertyType = '', location = '', assets = [], propertyFeatureList = [] } = {}) {
     this.id = id
     this.title = title
     this.description = description
     this.location = location
     this.assets = assets
     this.propertyType = propertyType
+    this.propertyFeatureList = propertyFeatureList
   }
 }
