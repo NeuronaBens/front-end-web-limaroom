73ddca1f6420024ed2d55b9aa9d7a7dd03bb061b
renato-17 <renato.avela.17@gmail.com>
2022-11-25 02:47:57 -0500

    fix[img]: fixing some image paths

--- src/rental/domain/enum/features.enum.js
+++ src/rental/domain/enum/features.enum.js
@@ -2,52 +2,52 @@ const FEATURES = {
   WIFI: {
     name: 'Wi-Fi',
     type: 'service',
-    icon: '/src/rental/ui/assets/wifi.svg'
+    icon: 'https://firebasestorage.googleapis.com/v0/b/meet-your-roommate-c7ed7.appspot.com/o/common%2Ficons%2Fwifi.svg?alt=media&token=8d9153b5-dd69-4d10-a5ac-defb5e1e12bd'
   },
   PARKING: {
     name: 'Parking',
     type: 'service',
-    icon: '/src/rental/ui/assets/parking.svg'
+    icon: 'https://firebasestorage.googleapis.com/v0/b/meet-your-roommate-c7ed7.appspot.com/o/common%2Ficons%2Fparking.svg?alt=media&token=a67a8da7-e2da-4fee-8a2d-4b831d1a756a'
   },
   AIR_CONDITIONING: {
     name: 'Air Conditioner',
     type: 'appliance',
-    icon: '/src/rental/ui/assets/air-conditioning.svg'
+    icon: 'https://firebasestorage.googleapis.com/v0/b/meet-your-roommate-c7ed7.appspot.com/o/common%2Ficons%2Fair-conditioning.svg?alt=media&token=1bd25e3e-aec4-49e6-953d-cec87624c9ff'
   },
   TV: {
     name: 'TV',
     type: 'appliance',
-    icon: '/src/rental/ui/assets/tv.svg'
+    icon: 'https://firebasestorage.googleapis.com/v0/b/meet-your-roommate-c7ed7.appspot.com/o/common%2Ficons%2Ftv.svg?alt=media&token=bb022f26-f83f-47e3-b01d-6215aca7e852'
   },
   LAUNDRY: {
     name: 'Laundry',
     type: 'service',
-    icon: '/src/rental/ui/assets/laundry.svg'
+    icon: 'https://firebasestorage.googleapis.com/v0/b/meet-your-roommate-c7ed7.appspot.com/o/common%2Ficons%2Flaundry.svg?alt=media&token=1f128c56-b320-4b1e-af3b-4f0e95244494'
   },
   ELEVATOR: {
     name: 'Elevator',
     type: 'service',
-    icon: '/src/rental/ui/assets/elevator.svg'
+    icon: 'https://firebasestorage.googleapis.com/v0/b/meet-your-roommate-c7ed7.appspot.com/o/common%2Ficons%2Felevator.svg?alt=media&token=686a8597-d37c-4561-9fd5-0f5a4dd7b57b'
   },
   POOL: {
     name: 'Pool',
     type: 'service',
-    icon: '/src/rental/ui/assets/pool.svg'
+    icon: 'https://firebasestorage.googleapis.com/v0/b/meet-your-roommate-c7ed7.appspot.com/o/common%2Ficons%2Fpool.svg?alt=media&token=4b13e717-3a76-4af1-b23d-a28bd71a1d96'
   },
   GYM: {
     name: 'Gym',
     type: 'service',
-    icon: '/src/rental/ui/assets/gym.svg'
+    icon: 'https://firebasestorage.googleapis.com/v0/b/meet-your-roommate-c7ed7.appspot.com/o/common%2Ficons%2Fgym.svg?alt=media&token=a1bee4df-40e0-420d-8c91-f21162352403'
   },
   PETS: {
     name: 'Pets',
     type: 'term',
-    icon: '/src/rental/ui/assets/pets.svg'
+    icon: 'https://firebasestorage.googleapis.com/v0/b/meet-your-roommate-c7ed7.appspot.com/o/common%2Ficons%2Fpets.svg?alt=media&token=adddb05d-9981-4e06-83d8-a5c05377c5db'
   },
   SMOKING: {
     name: 'Smoking',
     type: 'term',
-    icon: '/src/rental/ui/assets/smoking.svg'
+    icon: 'https://firebasestorage.googleapis.com/v0/b/meet-your-roommate-c7ed7.appspot.com/o/common%2Ficons%2Fsmoking.svg?alt=media&token=a920f67f-b5fd-4e3d-b858-387feab3bd4f'
   }
 }
 
--- src/rental/ui/components/student/offer.component.vue
+++ src/rental/ui/components/student/offer.component.vue
@@ -1,7 +1,17 @@
 <template >
   <div class="offer__description">
     <h2>Aditional Information</h2>
-    <p><span class="fw-bold">Owner:</span> {{ owner.name }} {{ owner.surname }}</p>
+    <div class="owner">
+      <div class="owner__image">
+        <img :src="owner.photoUrl" alt="">
+      </div>
+      <div class="owner__name">
+        <p><span class="fw-bold">Name: </span>{{ owner.name }} </p>
+        <p><span class="fw-bold">Lastname: </span>{{ owner.surname }}</p>
+      </div>
+
+    </div>
+
     <p><span class="fw-bold">Conditions:</span> {{ offer.conditions }}</p>
     <p><span class="fw-bold">Offer Dateline:</span> {{ new Date(offer.lifecycle.endAt).toLocaleDateString() }}</p>
     <p><span class="fw-bold">Offer Status</span> {{ offer.status }}</p>
@@ -13,9 +23,31 @@
   h2 {
     margin-bottom: 1rem;
   }
+
   p {
     margin-bottom: 1rem;
     font-size: 1.8rem;
+  }
+
+  .owner {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+    gap: 3rem;
+  }
+
+  .owner__image {
+    width: 10rem;
+    height: 10rem;
+    margin-bottom: 1rem;
+    border-radius: 50%;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
   }
 }
 </style>
