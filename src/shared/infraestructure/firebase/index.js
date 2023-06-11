d124b98f42e7e242f664bdbdaf92dd208c765794
NeuronaBens <71675911+NeuronaBens@users.noreply.github.com>
2023-06-11 18:01:50 -0500

    Update index.js

--- src/shared/infraestructure/firebase/index.js
+++ src/shared/infraestructure/firebase/index.js
@@ -1,6 +1,17 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
+  apiKey: "AIzaSyD3daoodRrcriWgb6ML9ol-QxVHkBS9TnA",
+  authDomain: "limaroom-a2a26.firebaseapp.com",
+  projectId: "limaroom-a2a26",
+  storageBucket: "limaroom-a2a26.appspot.com",
+  messagingSenderId: "239713403758",
+  appId: "1:239713403758:web:8bcd1195a026006207377f",
+  measurementId: "G-TR1RQSZCX2"
+};
+
+/*
+const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY ?? '',
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ?? '',
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ?? '',
@@ -9,6 +20,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID ?? '',
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ?? ''
 }
+*/
 
 const app = initializeApp(firebaseConfig)
 
